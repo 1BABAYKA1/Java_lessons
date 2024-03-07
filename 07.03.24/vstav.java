f8c7e18839ad49bb02c5be876fd2faf688292564
1BABAYKA1 <91975984+1BABAYKA1@users.noreply.github.com>
2024-03-07 19:59:59 +0300

    Create vstav.java

---
+++ 07.03.24/vstav.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 2, 4, 3, 1 };
+        insertionSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int currentElement = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && currentElement < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = currentElement;
+        }
+    }
+}
